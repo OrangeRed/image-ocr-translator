81db40dacf2289813cf1cc159147e59f49721c31
OlegAba <olegaba.developer@gmail.com>
2021-03-03 01:59:53 -0500

    Add route middleware to send client at root route

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const CONNECTION_URL = process.env.CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 
 // Middleware
+app.use(express.static('client'));
 app.use(cors());
 app.use(express.json());
 
@@ -19,6 +20,11 @@ app.use(express.json());
 const endpoint = 'api';
 app.use(`/${ endpoint }/user`, authRoute);
 app.use(`/${ endpoint }/posts`, postRoute);
+
+// Handles any requests that don't match
+app.get('*', (_, res) => {
+  res.sendFile('client/index.html', { root: '.' });
+});
 
 // Connect to Database
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
