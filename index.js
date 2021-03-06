84d7c80e2c4aea736a08af365ca8083a01a8083e
Dmitriy <congo.dimtree@gmail.com>
2021-03-06 11:48:28 -0500

    moved translate functionality to its own route

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import fetch from 'node-fetch';
 import dotenv from 'dotenv';
-import translate from "translate";
 import authRoute from './routes/auth.js';
 import postRoute from './routes/posts.js';
+import translateRoute from './routes/translate.js';
 
 
 dotenv.config();
@@ -23,69 +23,7 @@ app.use(express.static('client'));
 const endpoint = 'api';
 app.use(`/${ endpoint }/user`, authRoute);
 app.use(`/${ endpoint }/posts`, postRoute);
-
-// Google Translate API call
-app.get('/api/translate/google/:source/:target/:input', async (request, response) => {
-  const source = request.params.source;
-  const target = request.params.target;
-  const input = request.params.input;
-
-  const google_translate_url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${source}&tl=${target}&dt=t&q=${input}`;
-  const google_translate = await fetch(google_translate_url)
-  .then(async response => {
-    const google_translate_json = await response.json();
-    return google_translate_json[0][0][0];
-  })
-  .catch(err => {
-    console.log("\nGoogle translate error");
-    console.log(err);
-    return "";
-  });
-
-  response.json(google_translate);
-});
-
-// MyMemory Translate API call
-app.get('/api/translate/myMemory/:source/:target/:input', async (request, response) => {
-  const source = request.params.source;
-  const target = request.params.target;
-  const input = request.params.input;
-
-  // myMemory API call
-  const myMemory_url = `https://api.mymemory.translated.net/get?q=${input}&langpair=${source}|${target}`;
-  const myMemory = await fetch(myMemory_url)
-  .then(async response => {
-    const myMemory_json = await response.json();
-    return myMemory_json.responseData.translatedText;
-  })
-  .catch(err => {
-    console.log("\nmyMemory error");
-    console.log(err);
-    return "";
-  });
-
-  response.json(myMemory);
-});
-
-// Libre Translate call 
-app.get('/api/translate/libre/:source/:target/:input', async (request, response) => {  
-  const source = request.params.source;
-  const target = request.params.target;
-  const input = request.params.input;
-
-  const libre = await translate(input, { 
-    to: target, 
-    from: source,
-    engine: "libre"
-  })
-  .catch(err => {
-    console.log("\nLibre error");
-    console.log(err);
-    return "";
-  });
-
-  response.json(libre);
-});
+app.use(`/${ endpoint }/translate`, translateRoute);
 
 // Connect to Database
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
