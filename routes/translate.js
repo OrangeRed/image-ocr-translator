2b1be8ba8e34482dcfb6a859a70794ed62ee2108
Dmitriy <congo.dimtree@gmail.com>
2021-03-09 12:54:49 -0500

    added url encoding to make sure APIs can handle special characters

--- routes/translate.js
+++ routes/translate.js
@@ -11,7 +11,7 @@ translator.get('/google/:source/:target/:input', async (request, response) => {
     const input = request.params.input;
 
     const google_translate_url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=${source}&tl=${target}&dt=t&q=${input}`;
-    const google_translate = await fetch(google_translate_url)
+    const google_translate = await fetch(encodeURI(google_translate_url))
     .then(async response => {
         const google_translate_json = await response.json();
         return google_translate_json[0][0][0];
@@ -33,7 +33,7 @@ translator.get('/myMemory/:source/:target/:input', async (request, response) => 
 
     // myMemory API call
     const myMemory_url = `https://api.mymemory.translated.net/get?q=${input}&langpair=${source}|${target}`;
-    const myMemory = await fetch(myMemory_url)
+    const myMemory = await fetch(encodeURI(myMemory_url))
     .then(async response => {
         const myMemory_json = await response.json();
         return myMemory_json.responseData.translatedText;
