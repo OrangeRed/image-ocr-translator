abd8056ed86dcfe9f3f9fef8333a21c40c90ad36
Daniel Nash <dnl.nash@gmail.com>
2021-04-24 19:42:05 -0400

    fixed double async calls on ocr and added ocr test script

--- services/ocr.js
+++ services/ocr.js
@@ -1,5 +1,9 @@
-import { createWorker } from 'tesseract.js';
 import fs from 'fs';
+import {createWorker,createScheduler, setLogging} from 'tesseract.js';
+const cachePath = './tessdata';
+  if (!fs.existsSync(cachePath)) fs.mkdirSync(cachePath, { recursive: true });
+
+
 
 /*
 DOCS:
@@ -8,31 +12,24 @@ https://golb.hplar.ch/2019/07/ocr-with-tesseractjs.html
 https://betterprogramming.pub/beginners-guide-to-tesseract-ocr-using-python-10ecbb426c3d
 */
 
-const cachePath = './tessdata';
 
-const worker = createWorker({
-  cachePath: cachePath,
 
   // TODO: https://stackoverflow.com/questions/10183998/how-to-connect-formidable-file-upload-to-socket-io-in-node-js
-  logger: msg => console.log(msg),
-  errorHandler: err => console.log(err)
-});
 
 const ocr = async (filePath, lang) => {
-  // Check if file exists 
-  if (!fs.existsSync(filePath)) return console.log("No file found in the specified path");
-
-  // Create cache directory if one does not exist
-  fs.mkdirSync(cachePath, { recursive: true });
-
+  if (!lang) lang='eng';
+//setLogging(true);
+  const worker = createWorker({
+  cachePath: cachePath,
+  //logger: msg => console.log(msg),
+  //errorHandler: err => console.log(err)
+  });
   await worker.load();
   await worker.loadLanguage(lang);
   await worker.initialize(lang);
-
-  const data = await worker.recognize(filePath);
+const { data: { text } } = await worker.recognize(filePath);
   await worker.terminate();
-
-  return data
+  return(text);
 };
 
 export default ocr;
