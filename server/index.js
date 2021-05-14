db0d80c81c859e757d8a195e98ec8184c8b23269
OlegAba <olegaba.developer@gmail.com>
2021-05-14 03:15:40 -0400

    Add dev script to server that will build client before starting

--- server/index.js
+++ server/index.js
@@ -2,21 +2,22 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path'
 import userRoute from './routes/userRoute.js';
 import translateRoute from './services/translate.js';
 import docRoute from './routes/docRoute.js'
 
 dotenv.config();
 
-const app = express();
-const CONNECTION_URL = process.env.CONNECTION_URL;
-const PORT = process.env.PORT || 5000;
-const GOOGLE_TRANSLATE_API_KEY = process.env.GOOGLE_TRANSLATE_API_KEY || "SAMPLE_GOOGLE_TRANSLATE_API_KEY";
+const app = express()
+const CONNECTION_URL = process.env.CONNECTION_URL
+const PORT = process.env.PORT
+const __dirname = path.resolve(path.dirname(''))
 
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use(express.static('client'));
+app.use(express.static(path.join(__dirname, '../client/build')))
 
 // Route Middleware
 const endpoint = 'api'
@@ -27,13 +28,13 @@ app.use(`/${ endpoint }/translate`, translateRoute)
 // Handles any requests that don't match
 app.get('*', (_, res) => {
   // TODO: Add a 404 not found page
-  res.sendFile('client/index.html', { root: '.' });
-});
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
 
 // Connect to Database
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   // Listen for server connections
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${ PORT }`)))
-  .catch((error) => console.log(error.message));
+  .catch((error) => console.log(error.message))
 
-mongoose.set('useFindAndModify', false);
+mongoose.set('useFindAndModify', false)
